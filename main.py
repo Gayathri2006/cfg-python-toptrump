533d5cd99f0097f2497c51e7cee10abd9f353dc8
Gayathri2006 <58569203+Gayathri2006@users.noreply.github.com>
2019-12-05 19:50:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-test
+score = 0
+import random
+computer_num = (random.randint(0,25))
+#line 3 chooses a random number for an id
+#someone will need to use an api here to give the computer_num an ID
+#here the user needs to get assigned a random top trump card with statistics
+stat = input("What stat do you want to use? yearOfBirth / Wandlength / Age / Ancestry")
+#line 6 asks for the users card
+if user > computer:
+    score = score +1
+#user and computer aren't defined yet but will be the player and computers statistic
